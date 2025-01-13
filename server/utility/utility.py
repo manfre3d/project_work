41b932eaaf4f4971a9a447164f863310ed31a09a
Manfredi Piraino <manfredipiraino@gmail.com>
2025-01-13 14:05:26 +0100

    add: refactor utility

--- server/utility/utility.py
+++ server/utility/utility.py
@@ -1,10 +1,15 @@
-def _set_headers(handler, code=200, response_data={},content_type="application/json"):
+def _set_headers(handler, code=200, response_data=b'', content_type="application/json", extra_headers=None, origin="http://localhost:8000"):
     handler.send_response(code)
-    handler.send_header("Access-Control-Allow-Origin", "*")
-    # impostazione tipo response json
+    handler.send_header("Access-Control-Allow-Origin", origin)
     handler.send_header("Content-Type", content_type)
-    # impostazione content_length
+    handler.send_header("Accept", content_type)
+    handler.send_header("Access-Control-Allow-Credentials", "true")
+    handler.send_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    handler.send_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    if extra_headers:
+        for header, value in extra_headers.items():
+            handler.send_header(header, value)
+    
     content_length = str(len(response_data))
-    handler.send_header("Content-Length", content_length) 
-
+    handler.send_header("Content-Length", content_length)
     handler.end_headers()
